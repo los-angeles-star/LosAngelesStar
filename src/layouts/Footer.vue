9691990a6aa4873d654b963e532c13ff9e6176e2
Alexander Bell <alexbell@daviselen.com>
2019-03-01 18:07:48 -0800

    Move Footer to separate component

---
+++ src/layouts/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+	<div slot-scope="{ footer }" class="container">
+		<small>&copy; <time datetime="1851-05-17">1851</time>&ndash;<time datetime="1864-10-01">1864</time>, <time datetime="1868-05-16">1868</time>&ndash;1879, 2019</small>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Footer',
+}
+</script>
