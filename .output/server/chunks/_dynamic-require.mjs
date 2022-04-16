37ad1bb86fa3b6ea54faded879bca43a6b0d8fc5
pixelasticity <contact@alexanderbell.info>
2022-04-15 18:37:23 -0700

    We should not track the .output directory

--- .output/server/chunks/_dynamic-require.mjs
+++
@@ -1,9 +0,0 @@
-const dynamicChunks = {
- ['pages/Index.js']: () => import('./app/pages/Index.mjs').then(function (n) { return n.I; })
-};
-
-function dynamicRequire(id) {
-  return dynamicChunks[id]();
-}
-
-export { dynamicRequire as default };
