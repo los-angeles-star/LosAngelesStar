1f45efcaa9d6f32286f5bb4d12699b28f1763c3c
pixelasticity <contact@alexanderbell.info>
2022-04-16 12:20:10 -0700

    Add translations for English and Spanish

---
+++ lang/en.js
@@ -0,0 +1,16 @@
+export default async (context, locale) => {
+  return await Promise.resolve({
+    los_angeles: 'Los Angeles',
+    home: 'Home',
+    about: 'About',
+    'weather': {
+      'svg': {
+        today: "Today's weather:"
+      },
+      current_temp: 'Current temperature:',
+      high_temp: 'Today’s high temperature:',
+      low_temp: 'Today’s low temperature:',
+      degrees_f: 'degrees Fahrenheit'
+    }
+  })
+}
---
+++ lang/es.js
@@ -0,0 +1,16 @@
+export default async (context, locale) => {
+  return await Promise.resolve({
+    los_angeles: 'Los Ángeles',
+    home: 'Inicio',
+    about: 'Sobre',
+    'weather': {
+      'svg': {
+        today: "El clima de hoy:"
+      },
+      current_temp: 'La temperatura de ora:',
+      high_temp: 'La temperatura alta de hoy:',
+      low_temp: 'La temperatura baja de hoy:',
+      degrees_f: 'grados Fahrenheit'
+    }
+  })
+}
