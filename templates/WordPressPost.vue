40eb17ed22c1cf687ff1bdb7a208260d9ec0fee4
pixelasticity <contact@alexanderbell.info>
2022-04-15 22:18:39 -0700

    Add article styles

--- templates/WordPressPost.vue
+++ templates/WordPressPost.vue
@@ -35,8 +35,52 @@ export default {
 </script>
 
 <style lang="scss">
+$content--width: 688px;
+
 :where(img, svg, video) {
   max-width: 100%;
   height: auto;
+}
+
+h3 {
+  font-size: 1.25em;
+}
+
+blockquote {
+  max-width: 640px;
+  margin-left: calc((100% - $content--width + 96px) / 2);
+  font-size: 1.375em;
+  font-weight: bold;
+
+  &::before {
+    content: '\201C';
+    font-size: 2.5em;
+    position: absolute;
+    left: -0.72727em;
+    top: -0.29091em;
+  }
+}
+
+.h-entry {
+  h1 {
+    font-size: 2.5em;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: $content--width;
+    letter-spacing: -0.0125em;
+  }
+
+  main {
+    & > * {
+      max-width: $content--width;
+      margin-left: calc((100% - $content--width) / 2);
+    }
+
+    & > p,
+    & > ul {
+      font-size: 1.25em;
+      line-height: 1.6;
+    }
+  }
 }
 </style>
