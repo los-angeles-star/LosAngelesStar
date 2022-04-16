eeaa8644a4541fd428baa50f78c7dfcdc3ad14ca
pixelasticity <contact@alexanderbell.info>
2022-04-15 18:40:02 -0700

    Remove /blog/ from all URLs

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ let dynamicRoutes = () => {
   const routes = axios
     .get(Config.wpDomain + "/wp/v2/posts?page=1&per_page=20")
     .then(res => {
-      return res.data.map(post => `/blog/${post.slug}`)
+      return res.data.map(post => `/${post.slug}`)
     })
   console.log(routes)
   return routes
