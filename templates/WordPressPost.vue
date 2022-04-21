a09a7e193cba60bcf9c993b65f989f15b8c21d18
pixelasticity <contact@alexanderbell.info>
2022-04-21 01:53:16 -0700

    Change slug to link

--- templates/WordPressPost.vue
+++ templates/WordPressPost.vue
@@ -2,7 +2,7 @@
   <article :key="id" class="h-entry" :class="'post-'+id" role="main" itemscope="" itemtype="https://schema.org/NewsArticle">
     <header>
       <h1 v-html="title.rendered" itemprop="headline"/>
-      <meta itemprop="url" :content="slug">
+      <meta itemprop="url" :content="link">
       <article-date-line :date="date" :modified="modified" />
       <article-by-line v-if="author" :key="author" :authorId="author" />
     </header>
