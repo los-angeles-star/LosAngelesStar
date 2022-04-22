740a38905f113210a503e5c066bcde5b169d5108
pixelasticity <contact@alexanderbell.info>
2022-04-22 00:24:08 -0700

    Remove thumbnailUrl meta tag
    It's been moved to the featured media component

--- templates/WordPressPost.vue
+++ templates/WordPressPost.vue
@@ -6,7 +6,6 @@
       <article-date-line :date="date" :modified="modified" />
       <article-by-line v-if="author" :key="author" :authorId="author" />
     </header>
-    <meta itemprop="thumbnailUrl" :content="featured_media">
     <article-featured-media v-if="featured_media" :key="featured_media" :featuredMedia="featured_media" />
     <main v-html="content.rendered" itemprop="articleBody"/>
   </article>
