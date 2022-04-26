80c3431c383a21a2b51914e4be05ae2da6a837db
pixelasticity <contact@alexanderbell.info>
2022-04-26 14:25:57 -0700

    Stash unused filters for now

--- plugins/dateFormat.js
+++ plugins/dateFormat.js
@@ -1,7 +1,21 @@
 import Vue from "vue"
+import dayjs from 'dayjs'
 
 Vue.filter("formatDate", value => {
   return new Date(value).toISOString('en-US', {
     timeZone: 'America/Los_Angeles'
   })
+})
+
+Vue.filter("currentDate", value => {
+  return new Date(value).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'America/Los_Angeles'
+  })
+})
+
+Vue.filter("formatDateDay", value => {
+  return dayjs(value).format('dddd, MMMM D, YYYY');
 })
