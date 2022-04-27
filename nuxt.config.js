ba284d6fb01da9cdd55820d4b6c51ecd4d302e98
pixelasticity <contact@alexanderbell.info>
2022-04-26 19:12:51 -0700

    Formatting

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,114 +13,114 @@ let dynamicRoutes = () => {
 }
 
 export default defineNuxtConfig({
-  /*
-   ** Headers
-   ** Common headers are already provided by @nuxtjs/pwa preset
-   */
-  head: {
-    htmlAttrs: {
-      dir: 'ltr'
-    },
-    meta: [
-      {
-        hid: 'description',
-        name: 'description',
-        content: Config.appDescription || ''
-      }
-    ],
-    link: [
-      {
-        hid: 'apple-touch-icon',
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png'
-      }
-    ],
-    titleTemplate: titleChunk => {
-      // If undefined or blank then we don't need the hyphen
-      return titleChunk ? `${titleChunk} - Los Angeles Star` : "Los Angeles Star";
-    }
-  },
-  css: ['~/node_modules/sanitize.css'],
-  components: true,
-  buildModules: [
-    '@nuxtjs/pwa',
-    '@nuxtjs/google-fonts',
-    '@nuxtjs/axios',
-    '@nuxtjs/style-resources',
-    '@nuxtjs/i18n',
-    '@nuxtjs/stylelint-module'
-  ],
-  pwa: {},
-  googleFonts: {
-    display: 'swap',
-    useStylesheet: true,
-    inject: true,
-    download: true,
-    families: {
-      'Old+Standard+TT': true,
-    }
-  },
-  axios: {
-    baseURL: 'http://localhost:3000',
-  },
-  plugins: [
-    "~/plugins/posts.server.js",
-    "~/plugins/dateFormat.js"
-  ],
-  generate: {
-    routes: dynamicRoutes
-  },
-  styleResources: {
-    scss: [
-      './assets/vars/*.scss',
-      './assets/abstracts/_mixins.scss' // use underscore "_" & also file extension ".scss"
-    ],
-    hoistUseStatements: true  // Hoists the "@use" imports. Applies only to "sass", "scss" and "less". Default: false.
-  },
-  i18n: {
-    defaultLocale: 'en',
-    locales: [
-      { name: 'Español', code: 'es', iso: 'es-US', file: 'es.js', dir: 'ltr' },
-      { name: 'English', code: 'en', iso: 'en-US', file: 'en.js', dir: 'ltr' }
-    ],
-    langDir: 'lang/',
-    pages: {
-      about: {
-        en: '/about',
-        es: '/sobre'
-      }
-    },
-    vueI18n: {
-      fallbackLocale: 'en',
-      dateTimeFormats: {
-        en: {
-          short: {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
-          },
-          long: {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-            weekday: 'long'
-          }
-        },
-        es: {
-          short: {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
-          },
-          long: {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-            weekday: 'long'
-          }
-        }
-      }
-    }
-  }
+	/*
+	 ** Headers
+	 ** Common headers are already provided by @nuxtjs/pwa preset
+	 */
+	head: {
+		htmlAttrs: {
+			dir: 'ltr'
+		},
+		meta: [
+			{
+				hid: 'description',
+				name: 'description',
+				content: Config.appDescription || ''
+			}
+		],
+		link: [
+			{
+				hid: 'apple-touch-icon',
+				rel: 'apple-touch-icon',
+				sizes: '180x180',
+				href: '/apple-touch-icon.png'
+			}
+		],
+		titleTemplate: titleChunk => {
+			// If undefined or blank then we don't need the hyphen
+			return titleChunk ? `${titleChunk} - Los Angeles Star` : "Los Angeles Star";
+		}
+	},
+	css: ['~/node_modules/sanitize.css'],
+	components: true,
+	buildModules: [
+		'@nuxtjs/pwa',
+		'@nuxtjs/google-fonts',
+		'@nuxtjs/axios',
+		'@nuxtjs/style-resources',
+		'@nuxtjs/i18n',
+		'@nuxtjs/stylelint-module'
+	],
+	pwa: {},
+	googleFonts: {
+		display: 'swap',
+		useStylesheet: true,
+		inject: true,
+		download: true,
+		families: {
+			'Old+Standard+TT': true,
+		}
+	},
+	axios: {
+		baseURL: 'http://localhost:3000',
+	},
+	plugins: [
+		"~/plugins/posts.server.js",
+		"~/plugins/dateFormat.js"
+	],
+	generate: {
+		routes: dynamicRoutes
+	},
+	styleResources: {
+		scss: [
+			'./assets/vars/*.scss',
+			'./assets/abstracts/_mixins.scss' // use underscore "_" & also file extension ".scss"
+		],
+		hoistUseStatements: true  // Hoists the "@use" imports. Applies only to "sass", "scss" and "less". Default: false.
+	},
+	i18n: {
+		defaultLocale: 'en',
+		locales: [
+			{ name: 'Español', code: 'es', iso: 'es-US', file: 'es.js', dir: 'ltr' },
+			{ name: 'English', code: 'en', iso: 'en-US', file: 'en.js', dir: 'ltr' }
+		],
+		langDir: 'lang/',
+		pages: {
+			about: {
+				en: '/about',
+				es: '/sobre'
+			}
+		},
+		vueI18n: {
+			fallbackLocale: 'en',
+			dateTimeFormats: {
+				en: {
+					short: {
+						year: 'numeric',
+						month: 'long',
+						day: 'numeric'
+					},
+					long: {
+						year: 'numeric',
+						month: 'long',
+						day: 'numeric',
+						weekday: 'long'
+					}
+				},
+				es: {
+					short: {
+						year: 'numeric',
+						month: 'long',
+						day: 'numeric'
+					},
+					long: {
+						year: 'numeric',
+						month: 'long',
+						day: 'numeric',
+						weekday: 'long'
+					}
+				}
+			}
+		}
+	}
 })
