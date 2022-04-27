8a2fe335ba8657325ff030fd9d278f05a17267b6
pixelasticity <contact@alexanderbell.info>
2022-04-26 23:45:49 -0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,8 +1,8 @@
 "use strict";
 
-const apiEndpoint = "css-tricks.com";
+export const API_Endpoint = "thenewinquiry.com";
 
-export default {
+export const Config = {
   appTitle: "Los Angeles Star",
   appTitleShort: "L.A. Star",
   appDescription: "Los Angeles’ first newspaper since 1852.",
@@ -10,11 +10,15 @@ export default {
   appBgColor: "#00172c ",
   appIcon: "static/icon.png",
   // these are the rest api endpoints and your wordpress url
-  client: `https://${apiEndpoint}`,
-  wpDomain: `https://${apiEndpoint}/wp-json`,
+  client: `https://${API_Endpoint}`,
+  wpDomain: `https://${API_Endpoint}/wp-json`,
   url: "https://losangelesstar.netlify.com/", // your website url
   // loadDbName: "[starter_wp]", // db name if needed
   api: {
     homePage: "/wp/v2/pages/2", // the [page_id] from WordPress
+    posts: "/wp-json/wp/v2/posts",
+    users: "/wp/v2/users/",
+    media: "/wp/v2/media/",
+    tags: "/wp-json/wp/v2/tags"
   }
 };
