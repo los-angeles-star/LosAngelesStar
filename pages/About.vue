d152efffb4f024f21dd40f7a159ef89b3456fdb9
pixelasticity <contact@alexanderbell.info>
2022-05-07 02:33:33 -0700

    Create About.vue

---
+++ pages/About.vue
@@ -0,0 +1,27 @@
+<template>
+  <article :key="page.id" :class="'page-'+page.id" role="main" itemscope="" itemtype="https://schema.org/AboutPage">
+		<header>
+			<h1 v-if="page.title" v-html="page.title.rendered" itemprop="headline"/>
+			<meta itemprop="url" :content="page.link">
+			<div v-if="page.excerpy" class="lead" v-html="page.excerpt.rendered" />
+			<article-featured-media v-if="page.featured_media" :key="page[0].featured_media" :featuredMedia="page[0].featured_media" />
+		</header>
+		<main v-if="page.content" v-html="page.content.rendered"/>
+	</article>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'About',
+  computed: mapState({
+  		page: 'currentPage'
+  }),
+	created() {
+		this.$store.dispatch('getCurrentPage', {
+      slug: this.$route.path.substring(1).split('/')[0]
+    });
+	}
+}
+</script>
