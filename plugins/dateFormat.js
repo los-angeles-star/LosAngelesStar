c392ba6736b4b6a44858748f2938cd13c1c6efbc
Alexander Bell <>
2023-01-13 23:01:45 -0800

    Update dateFormat.js
    The filter method was removed from Vue 3

--- plugins/dateFormat.js
+++ plugins/dateFormat.js
@@ -1,21 +1,24 @@
-import Vue from "vue"
 import dayjs from 'dayjs'
 
-Vue.filter("formatDate", value => {
-  return new Date(value).toISOString('en-US', {
-    timeZone: 'America/Los_Angeles'
-  })
-})
+const nuxtApp = useNuxtApp()
 
-Vue.filter("currentDate", value => {
-  return new Date(value).toLocaleDateString('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    timeZone: 'America/Los_Angeles'
-  })
-})
+nuxtApp.config.globalProperties.$filters = {
+  formatDate(value) {
+    return new Date(value).toISOString('en-US', {
+      timeZone: 'America/Los_Angeles'
+    })
+  }
 
-Vue.filter("formatDateDay", value => {
-  return dayjs(value).format('dddd, MMMM D, YYYY');
-})
+  currentDate(value) {
+    return new Date(value).toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      timeZone: 'America/Los_Angeles'
+    })
+  }
+
+  formatDateDay(value) {
+    return dayjs(value).format('dddd, MMMM D, YYYY');
+  }
+}
