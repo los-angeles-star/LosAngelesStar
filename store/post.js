8b60cd3476ec3be37a285f7b30dc1b9bad05034b
Alexander Bell <>
2023-01-14 11:49:13 -0800

    Convert post store to use Pinia

---
+++ store/post.js
@@ -0,0 +1,48 @@
+import { Config } from '~/assets/config'
+
+export const state = () => ({
+  post: {},
+})
+
+export const getters = {
+  post: state => state.post,
+}
+
+export const mutations = {
+  updatePost: (state, post) => {
+    state.post = post
+  }
+}
+
+export const actions = {
+  async getPost({ state, commit, dispatch }, data) {
+    if (state.post.length) return
+
+    try {
+      let post = await fetch(
+        `${Config.wpDomain}${Config.api.posts}?slug=${data.slug}`
+      ).then(res => res.json())
+
+      post = post
+        .filter(el => el.status === "publish")
+        .map(({ id, slug, title, excerpt, date, modified, author, categories, tags, content, featured_media, link }) => ({
+          id,
+          slug,
+          title,
+          excerpt,
+          date,
+          modified,
+          author,
+          categories,
+          tags,
+          content,
+          featured_media,
+          link
+        }))
+
+      commit("updatePost", post[0])
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
