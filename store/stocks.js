794a0670d72abdf6182a83dd1b0ff64789866b7c
Alexander Bell <>
2023-01-14 11:47:24 -0800

    Remove nuxt-bridge

--- store/stocks.js
+++ store/stocks.js
@@ -1,64 +1,60 @@
-import axios from 'axios'
+import { defineStore } from 'pinia'
 
-export const state = () => ({
-  marketSummary: {
-    0: {
-      shortName: 'S&P 500',
-      regularMarketTime: {
-        raw: 1637791540
+export const useStocksStore = defineStore('stocks', {
+  // a function that returns a fresh state
+  state: () => ({
+    marketSummary: {
+      0: {
+        shortName: 'S&P 500',
+        regularMarketTime: {
+          raw: 1637791540
+        },
+        regularMarketChangePercent: {
+          raw: 0,
+          fmt: '?.??%'
+        }
       },
-      regularMarketChangePercent: {
-        raw: 0,
-        fmt: '?.??%'
+      1: {
+        shortName: 'Nasdaq',
+        regularMarketTime: {
+          raw: 1637792159
+        },
+        regularMarketChangePercent: {
+          raw: 0,
+          fmt: '?.??%'
+        }
+      },
+      2: {
+        shortName: 'Dow',
+        regularMarketTime: {
+          raw: 1637791540
+        },
+        regularMarketChangePercent: {
+          raw: 0,
+          fmt: '?.??%'
+        }
       }
     },
-    1: {
-      shortName: 'Nasdaq',
-      regularMarketTime: {
-        raw: 1637792159
-      },
-      regularMarketChangePercent: {
-        raw: 0,
-        fmt: '?.??%'
+  }),
+  // optional actions
+  actions: {
+    async getMarketSummary({ state, commit }) {
+      if (state.marketSummary.length) return
+
+      try {
+        let marketSummary = await useFetch(
+          `/api/v6/finance/quote/marketSummary?lang=en&region=US`
+        ).then((response) => {
+          return response.data.marketSummaryResponse.result
+        })
+
+        commit("updateMarketSummary", marketSummary)
+      } catch (error) {
+        console.log(error.name, error.message)
       }
     },
-    2: {
-      shortName: 'Dow',
-      regularMarketTime: {
-        raw: 1637791540
-      },
-      regularMarketChangePercent: {
-        raw: 0,
-        fmt: '?.??%'
-      }
+    updateMarketSummary(state, marketSummary) => {
+      state.marketSummary = marketSummary
     }
-  },
+  }
 })
-
-export const getters = {
-  marketSummary: state => state.marketSummary,
-}
-
-export const mutations = {
-  updateMarketSummary: (state, marketSummary) => {
-    state.marketSummary = marketSummary
-  }
-}
-
-export const actions = {
-  async getMarketSummary({ state, commit }) {
-    if (state.marketSummary.length) return
-
-    try {
-      let marketSummary = await useFetch(
-        `/api/v6/finance/quote/marketSummary?lang=en&region=US`
-      ).then((response) => {
-        return response.data.marketSummaryResponse.result
-      })
-
-      commit("updateMarketSummary", marketSummary)
-    } catch (error) {
-      console.log(error.name, error.message)
-    }
-  }
-}
