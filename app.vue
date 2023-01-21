e1505d74d2878eb42f392232823b0529b193dad5
Alexander Bell <>
2023-01-21 13:24:14 -0800

    Create app.vue
    Nuxt 3 recommended set-up

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+</template>
+
+<script>
+import { useMainStore } from '@/stores/';
+
+export default defineNuxtComponent({
+  setup() {
+    const store = useMainStore();
+
+    store.getMeta()
+  }
+})
+</script>
